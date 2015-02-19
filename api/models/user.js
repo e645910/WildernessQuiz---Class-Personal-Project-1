c09de86997e322872aa2f77dfacf0dc08607ba3a
rwightman <rick.wightman@q.com>
2015-02-19 10:28:49 -0700

    auth working

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
-var bcyrpt = require('bcrypt');
+var bcrypt = require('bcrypt');
 var q = require('q');
+
 
 var schema = mongoose.Schema({
 	email: { type: String, required: true, unique: true },
@@ -9,18 +10,41 @@ var schema = mongoose.Schema({
 	age: Number
 });
 //serial middleware excutes after next call
-schema.pre('save' function(next){
+schema.pre('save', function(next){
 	//defines user as this 
 	var user = this;
-		if (!user.isModified) {
-			return next();
-		} //hashes a password -- bcrypt uses hash to help to incrypt the password (hash returns a numerical number from a string)
-	bcyrpt.genSalt(12, function(err, salt){
+	// only hash the password if it has been modified (or is new)
+	if (!user.isModified('password')) {
+		return next();
+	}
+	 // generate a salt (rainbow fish attacks and resist brute force attacks)
+	bcrypt.genSalt(12, function(err, salt){
+		if(err) {
 			return next(err);
-		} //store hash in the password DB
-	bycrpt.hash(user.password, salt, function(err, hash){
+		}
+			
+		bcrypt.hash(user.password, salt, function(err, hash){
 			user.password = hash;
 			return next();
+		});
+	}); //store hash number in the password DB
+	
+});
+ // now checks the password
+schema.methods.comparePassword = function(pass){
+	//expose the associated Promise to signal a successful or unsuccessful check, as well as the status of the task
+	var dfd = q.defer();
+	// Loads hash from the password DB
+	bcrypt.compare(pass, this.password, function(err, isMatch){
+		if(err) {
+			dfd.reject(err);
+		}
+		else {
+			dfd.resolve(isMatch);
+		}
+		
 	});
-	})
-})
+	return dfd.promise;
+};
+
+module.exports = mongoose.model('User', schema);
--- server.js
+++ server.js
@@ -5,36 +5,57 @@ var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 
+var User = require('./api/models/user');
+var UserController = require('./api/controllers/userCtrl')
 var app = express();
 
-mongoose.connect('mongodb://localhost/wildernessquiz');
+mongoose.connect('mongodb://localhost/wildernessQuiz', function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
 
 passport.use(new localStrategy({
+	//require the defualt username to the person's email address 
 	usernameField: 'email',
 	passwordField: 'password'
 }, 	function(username, password, done){
+	console.log(username)
 	User.findOne({ email: username }).exec().then(function(user){
+		console.log(user)
 		if(!user){
-			return done(null, false);
+			return done(null, false, { message: 'Incorrect username.' });
 		}
 		user.comparePassword(password).then(function(isMatch){
+			console.log(isMatch)
 			if(!isMatch){
-				return done(null, false);
+				return done(null, false, { message: 'Incorrect password.' });
 			}
 			return done(null, user);
+		}, function(err){
+			console.log(err)
 		});
+	}, function(err){
+		return done(err);
 	});
 }));
 
 passport.serializeUser(function(user, done){
 	//input user model (mongoose)
-	done(null, obj);
+	done(null, user);
 });
-passport.deserializeUser(function(user, done){
+passport.deserializeUser(function(obj, done){
 	//user object (json)
 	done(null, obj);
 });
 
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname+'/public'));
+
+app.use(session({secret: 'WILDSEEKRIT', 
+	saveUninitialized: true,
+    resave: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -43,39 +64,26 @@ app.post('/api/auth', passport.authenticate('local'), function(req, res){
 	return res.status(200).end();
 });
 
-app.use(bodyParser.json());
-app.use(express.static(__dirname+'/public'));
 
-app.use(session({secret: 'WILDSEEKRIT', 
-	saveUninitialized: true,
-    resave: true}));
 
-app.post('/api/register', function(res, req){
+app.post('/api/register', function(req, res){
 	//creates a new user
 	var newUser = new User(req.body);
 	newUser.save(function(err, user){
 		if(err) {
-			return res.status(500).end();
+			return res.status(500).json(err);
 		}
 		return res.json(user);
 	});
 });
 
-var isAuthed = function(req, res){
-	if(!req.isAuthenticated()){
-		return res.stataus(403).end();
-	}
-	return next();
-};
-var UserController = require('./api/controllers/userCtrl')
+// var isAuthed = function(req, res){
+// 	if(!req.isAuthenticated()){
+// 		return res.stataus(403).end();
+// 	}
+// 	return next();
+// };
 
-
-
-
-
-
-
-
-
+// app.post('/api/customer', UserController.post);
 
 app.listen(8000);
