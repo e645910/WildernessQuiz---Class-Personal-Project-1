7c003aef1e26a7d29638cff3066ce614855cebce
rwightman <rick.wightman@q.com>
2015-07-13 11:20:51 -0600

    updated css from http to https

--- public/js/controllers/navBar-Ctrl.js
+++ public/js/controllers/navBar-Ctrl.js
@@ -1,11 +1,11 @@
 angular.module('wildernessQuiz')
 .controller('navBarCtrl', function($scope, $rootScope, $location){
 	$scope.selectQuestionForm = function(){
-		if($rootScope.userId === '55037e6f1af1c6c56fa12a3d'){
-			$scope.questionPath = $location.path('/question')
-		}else {
-			alert('You must be a quiz admin to access this function!');
-		}
+		// if($rootScope.userId === '55037e6f1af1c6c56fa12a3d'){
+		// 	$scope.questionPath = $location.path('/question')
+		// }else {
+		// 	alert('You must be a quiz admin to access this function!');
+		// }
 	};
 	$scope.selectQuizForm = function(){
 		$scope.quizPath = $location.path('/quiz');
