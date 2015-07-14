f8aa9ff67e5aa889d03f424c0a85790a76c4bb3e
rwightman <rick.wightman@q.com>
2015-07-14 00:52:10 -0600

    updated admin feature for question

--- public/js/controllers/navBar-Ctrl.js
+++ public/js/controllers/navBar-Ctrl.js
@@ -1,11 +1,11 @@
 angular.module('wildernessQuiz')
 .controller('navBarCtrl', function($scope, $rootScope, $location){
 	$scope.selectQuestionForm = function(){
-		// if($rootScope.userId === '55037e6f1af1c6c56fa12a3d'){
+		if($rootScope.userId === '55a3d59e0a50aa1100c8b34f'){
 			$scope.questionPath = $location.path('/question')
-		// }else {
-		// 	alert('You must be a quiz admin to access this function!');
-		// }
+		}else {
+		 	alert('You must be a quiz admin to access this function!');
+		 }
 	};
 	$scope.selectQuizForm = function(){
 		$scope.quizPath = $location.path('/quiz');
