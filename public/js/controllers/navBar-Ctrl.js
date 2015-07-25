8c0b64522c48570c3183453defb3f98ce5e4c588
rwightman <rick.wightman@q.com>
2015-07-25 00:33:21 -0600

    update

--- public/js/controllers/navBar-Ctrl.js
+++ public/js/controllers/navBar-Ctrl.js
@@ -1,7 +1,7 @@
 angular.module('wildernessQuiz')
 .controller('navBarCtrl', function($scope, $rootScope, $location){
 	$scope.selectQuestionForm = function(){
-		if($rootScope.userId === '55a3d59e0a50aa1100c8b34f' || $rootScope.userId === '55037e6f1af1c6c56fa12a3d'){
+		if($rootScope.userId === '55af1daa99a1511100d2c7ae' || $rootScope.userId === '55037e6f1af1c6c56fa12a3d'){
 			$scope.questionPath = $location.path('/question')
 		}else {
 		 	alert('You must be a quiz admin to access this function!');
