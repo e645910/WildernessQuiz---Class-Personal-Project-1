6f0e91192c6e66673aaa5e8ecb61988ce6c3c971
rwightman <rick.wightman@q.com>
2015-07-26 01:36:47 -0600

    test

--- public/js/controllers/question-Ctrl.js
+++ public/js/controllers/question-Ctrl.js
@@ -1,67 +1,67 @@
-// angular.module('wildernessQuiz')
-// .controller('questionCtrl', function($scope, questionService){
-//  	$scope.clickSaveQuestion = function(){
-//  		questionService.saveQuestion(
-//  			$scope.question,
-//  			$scope.answer,
-//  			$scope.badAnswer1,
-//  			$scope.badAnswer2,
-//  			$scope.badAnswer3,
-//  			$scope.supportData,
-//  			$scope.image
-//  		)
-// 	 		$scope.question = '';
-// 		    $scope.answer = '';
-// 		    $scope.badAnswer1 = '';
-// 		    $scope.badAnswer2 = '';
-// 		    $scope.badAnswer3 = '';
-// 		    $scope.supportData = '';
-// 		    $scope.image = '';
-// 	 	};
+angular.module('wildernessQuiz')
+.controller('questionCtrl', function($scope, questionService){
+ 	$scope.clickSaveQuestion = function(){
+ 		questionService.saveQuestion(
+ 			$scope.question,
+ 			$scope.answer,
+ 			$scope.badAnswer1,
+ 			$scope.badAnswer2,
+ 			$scope.badAnswer3,
+ 			$scope.supportData,
+ 			$scope.image
+ 		)
+	 		$scope.question = '';
+		    $scope.answer = '';
+		    $scope.badAnswer1 = '';
+		    $scope.badAnswer2 = '';
+		    $scope.badAnswer3 = '';
+		    $scope.supportData = '';
+		    $scope.image = '';
+	 	};
 
-// //==================== * update records * ============================
+//==================== * update records * ============================
 
-// // this function will show the first question & answer to update	
-// 	$scope.clickGetQA = function(){
-//  		questionService.getQA()
-//  		.then(function(res){
-//  			$scope._id = res._id,
-// 	 		$scope.question = res.question;
-//  			$scope.answer = res.answer;
-//  			$scope.badAnswer1 = res.badAnswer1;
-//  			$scope.badAnswer2 = res.badAnswer2;
-//  			$scope.badAnswer3 = res.badAnswer3;
-//  			$scope.supportData = res.supportData;
-//  			$scope.image = res.image;
-//  		})
-//  	};
+// this function will show the first question & answer to update	
+	$scope.clickGetQA = function(){
+ 		questionService.getQA()
+ 		.then(function(res){
+ 			$scope._id = res._id,
+	 		$scope.question = res.question;
+ 			$scope.answer = res.answer;
+ 			$scope.badAnswer1 = res.badAnswer1;
+ 			$scope.badAnswer2 = res.badAnswer2;
+ 			$scope.badAnswer3 = res.badAnswer3;
+ 			$scope.supportData = res.supportData;
+ 			$scope.image = res.image;
+ 		})
+ 	};
 
-// // this function will show the next question & answer to update 
-//  	$scope.clickNextRecord = function(){
-//  		questionService.getNextQA()
-//  		.then(function(res){
-//  			$scope._id = res._id;
-//  			$scope.question = res.question;
-//  			$scope.answer = res.answer;
-//  			$scope.badAnswer1 = res.badAnswer1;
-//  			$scope.badAnswer2 = res.badAnswer2;
-//  			$scope.badAnswer3 = res.badAnswer3;
-//  			$scope.supportData = res.supportData;
-//  			$scope.image = res.image;
-//  		})
-//  	};
+// this function will show the next question & answer to update 
+ 	$scope.clickNextRecord = function(){
+ 		questionService.getNextQA()
+ 		.then(function(res){
+ 			$scope._id = res._id;
+ 			$scope.question = res.question;
+ 			$scope.answer = res.answer;
+ 			$scope.badAnswer1 = res.badAnswer1;
+ 			$scope.badAnswer2 = res.badAnswer2;
+ 			$scope.badAnswer3 = res.badAnswer3;
+ 			$scope.supportData = res.supportData;
+ 			$scope.image = res.image;
+ 		})
+ 	};
 
-// // this function updates the question & answer in the database
-// 	$scope.clickUpdateRecord = function(){
-// 		questionService.updateQA(
-// 			$scope._id,
-//  			$scope.question,
-//  			$scope.answer,
-//  			$scope.badAnswer1,
-//  			$scope.badAnswer2,
-//  			$scope.badAnswer3,
-//  			$scope.supportData,
-//  			$scope.image
-// 		)
-// 	}
-// });
+// this function updates the question & answer in the database
+	$scope.clickUpdateRecord = function(){
+		questionService.updateQA(
+			$scope._id,
+ 			$scope.question,
+ 			$scope.answer,
+ 			$scope.badAnswer1,
+ 			$scope.badAnswer2,
+ 			$scope.badAnswer3,
+ 			$scope.supportData,
+ 			$scope.image
+		)
+	}
+});
